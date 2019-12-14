eb10b9922733c70a33ac62ded6fbc88bf5212928
Panos <panos277@hotmail.com>
2019-12-15 00:40:27 +0200

    Add configuration service

---
+++ configurationservice/src/test/java/eu/ioannidis/unisys/configurationservice/ConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.ioannidis.unisys.configurationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
